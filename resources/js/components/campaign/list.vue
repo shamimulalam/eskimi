8dde24238f36251338d3198456b59b297530873b
Shamimul Alam <bd.shawon1991@gmail.com>
2021-01-16 17:20:04 +0600

    Reusuable component done

--- resources/js/components/campaign/list.vue
+++ resources/js/components/campaign/list.vue
@@ -26,10 +26,7 @@
                 >
 
                     <template v-slot:cell(actions)="data">
-                        <b-button
-                            @click="viewCreatives(data.item)"
-                            variant="success"
-                        >Creatives</b-button>
+                        <showCampaignCreatives :campaign="data.item"></showCampaignCreatives>
                         <b-button
                             @click="$router.push({name: 'campaign.edit',params: {id: data.item.id}})"
                             variant="info"
@@ -43,16 +40,6 @@
                     aria-controls="my-table"
                 ></b-pagination>
             </b-col>
-
-
-            <b-modal ref="creativeModal" hide-footer>
-                <template #modal-title>
-                    Creatives for {{ modal.title }}
-                </template>
-                <div class="d-block text-center">
-                    <img v-for="creative in modal.creatives" :src="'../storage/'+creative.path" width="100%">
-                </div>
-            </b-modal>
         </b-row>
         <b-row v-else>
             <b-col>No campaign found</b-col>
@@ -62,9 +49,12 @@
 
 <script>
 import moment from "moment";
-
+import showCampaignCreatives from '../reusables/showCampaignCreatives'
 export default {
     name: "list",
+    components: {
+        showCampaignCreatives
+    },
     data() {
         return {
             campaigns : {},
@@ -102,11 +92,6 @@ export default {
                     }
                 })
             }
-        },
-        viewCreatives(campaign){
-            this.modal.title = campaign.name
-            this.modal.creatives = campaign.creatives
-            this.$refs['creativeModal'].show();
         }
 
     },
---
+++ resources/js/components/reusables/showCampaignCreatives.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <b-button
+            @click="viewCreatives()"
+            variant="success"
+        >Creatives</b-button>
+        <b-modal ref="creativeModal" hide-footer>
+            <template #modal-title>
+                Creatives for {{ title }}
+            </template>
+            <div class="d-block text-center">
+                <img v-for="creative in creatives" :src="'../storage/'+creative.path" width="100%">
+            </div>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "showCampaignCreatives",
+    data() {
+        return {
+            title: '',
+            creatives: '',
+        }
+    },
+    props: [
+        "campaign"
+    ],
+    methods: {
+        viewCreatives(){
+            this.title = this.campaign.name
+            this.creatives = this.campaign.creatives
+            this.$refs['creativeModal'].show();
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
